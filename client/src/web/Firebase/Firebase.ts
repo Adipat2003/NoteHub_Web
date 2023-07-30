449025e9991efb95f03f42036a7adac30c649080
lohitm2 <lohit308@gmail.com>
2023-07-29 20:10:49 -0500

    added .env

--- client/src/web/Firebase/Firebase.ts
+++ client/src/web/Firebase/Firebase.ts
@@ -3,20 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage"
 import { getDatabase } from 'firebase/database';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBUVJVqlx_5VSQ9LPofvZXb1koFV-ShWWM",
-    authDomain: "notesapp-bf7b2.firebaseapp.com",
-    databaseURL: "https://notesapp-bf7b2-default-rtdb.firebaseio.com",
-    projectId: "notesapp-bf7b2",
-    storageBucket: "notesapp-bf7b2.appspot.com",
-    messagingSenderId: "397140551026",
-    appId: "1:397140551026:web:79a823b580315ceea9e9eb",
-    measurementId: "G-2XCYK2QHG0"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
