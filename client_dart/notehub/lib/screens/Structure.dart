e266122f612249292d0f2edd0e6e042b5a1e1358
lohitm2 <lohit308@gmail.com>
2023-08-28 00:54:30 -0500

    Made Changes

---
+++ client_dart/notehub/lib/screens/Structure.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Structure extends StatefulWidget {
+  const Structure({super.key});
+
+  @override
+  State<Structure> createState() => _StructureState();
+}
+
+class _StructureState extends State<Structure> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My tickets"),
+      ),
+    );
+  }
+}
